af17c58bef18e41e7a2fc9aa2979a094bb4d8694
Yukit02 <yukijobit02@gmail.com>
2022-03-09 13:51:15 +0900

    [feature] implement of mining

--- models/blockchain.go
+++ models/blockchain.go
@@ -1,9 +1,21 @@
 package models
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Blockchain struct {
 	transactionPool []*Transaction
 	chain []*Block
+	blockchainAddress string
 }
+
+const (
+	MINING_DIFFICULTY = 3
+	MINING_SENDER = "THE BLOCKCHAIN NODE"
+	MINING_REWARD = 1.0
+)
 
 func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash, bc.transactionPool)
@@ -14,9 +26,10 @@ func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	return b
 }
 
-func NewBlockchain() *Blockchain {
+func NewBlockchain(blockchainAddress string) *Blockchain {
 	b := &Block{}
 	bc := &Blockchain{}
+	bc.blockchainAddress = blockchainAddress
 	bc.CreateBlock(0, b.ToHash())
 
 	return bc
@@ -29,4 +42,33 @@ func (bc *Blockchain) LastBlock() *Block {
 func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
 	t := NewTransaction(sender, recipient, value)
 	bc.transactionPool = append(bc.transactionPool, t)
+}
+
+func (bc *Blockchain) IsValidProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
+	zeros := strings.Repeat("0", difficulty)
+	guessBlock := Block{nonce, previousHash, 0, transactions}
+	guessHashStr := fmt.Sprintf("%x", guessBlock.ToHash())
+
+	return guessHashStr[:difficulty] == zeros
+}
+
+func (bc *Blockchain) ProofOfWork() int {
+	nonce := 0
+	previousHash := bc.LastBlock().ToHash()
+
+	for !bc.IsValidProof(nonce, previousHash, bc.transactionPool, MINING_DIFFICULTY) {
+		nonce += 1
+	}
+
+	return nonce
+}
+
+func (bc *Blockchain) Mining() bool {
+	bc.AddTransaction(MINING_SENDER, bc.blockchainAddress, MINING_REWARD)
+
+	nonce := bc.ProofOfWork()
+	previousHash := bc.LastBlock().ToHash()
+	bc.CreateBlock(nonce, previousHash)
+
+	return true
 }
