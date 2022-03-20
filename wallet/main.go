19f6ef7c6609096129a15c8f9e630022ea8b7773
Yukit02 <yukijobit02@gmail.com>
2022-03-20 10:59:38 +0900

    [change] build port number

--- wallet/main.go
+++ wallet/main.go
@@ -12,7 +12,7 @@ func init() {
 }
 
 func main() {
-	port := flag.Uint("port", 8080, "TCP Port Number for Wallet Server")
+	port := flag.Uint("port", 8000, "TCP Port Number for Wallet Server")
 	gateway := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
 
 	app := wlcontrollers.NewWalletServer(uint16(*port), *gateway)
